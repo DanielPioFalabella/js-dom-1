36e393e9034727d555e33e2f6f1fb751dc3ae9a8
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2025-12-10 15:27:44 +0100

    descrizione dei passaggi

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // associo una const all'elemento del bottone
 const pulsanteAccendi = document.getElementById("pulsanteAccendi")
+
+// associo una let all'img iniziale pk poi dovrà cambiarmi
 let lampSpenta = document.getElementById("lampSpenta")
 
 // do una condizione al bottone quando ci clicco
 pulsanteAccendi.addEventListener("click", 
     function accendiLampadina (){
         if(lampSpenta.src.includes("white_lamp.png")){
-            lampSpenta.src = "./imgs/yellow_lamp.png"
-            pulsanteAccendi.innerText = "Spegni"
+            lampSpenta.src = "./imgs/yellow_lamp.png" // in questo modo sto cambiando l'src dell'img da vedere in pagina
+            pulsanteAccendi.innerText = "Spegni" // qui invece cambio il testo del btn
         } else {
-            lampSpenta.src = "./imgs/white_lamp.png"
+            lampSpenta.src = "./imgs/white_lamp.png" // altrimenti mi ritorna l'img iniziale
             pulsanteAccendi.innerText = "Accendi"
         }
     }
